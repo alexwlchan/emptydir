37767809eacf097dd9c118f46e0837ce90ee080e
Alex Chan <alex@alexwlchan.net>
2024-06-18 21:41:46 +0100

    Pull the bulk of the logic out into a standalone function

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 #![deny(warnings)]
 
 use std::fs;
+use std::path::Path;
 
 use colored::*;
 use walkdir::WalkDir;
 
 mod can_be_deleted;
 
-fn main() -> Result<(), std::io::Error> {
-    let iterator = WalkDir::new(".")
+/// Recurse through a given root directory, and delete any "empty" directories.
+///
+/// Returns the number of directories deleted.
+///
+fn emptydir(root: &Path) -> u32 {
+    let iterator = WalkDir::new(root)
         .contents_first(true)
         .into_iter()
         .filter_map(|e| e.ok())
         .filter(|e| e.file_type().is_dir())
         .filter(|e| can_be_deleted::can_be_deleted(e.path()));
 
-    let mut count_deleted = 0;
+    let mut count_deleted: u32 = 0;
 
     for entry in iterator {
         match fs::remove_dir_all(entry.path()) {
@@ -26,6 +31,12 @@ fn main() -> Result<(), std::io::Error> {
             _ => (),
         };
     }
+
+    count_deleted
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let count_deleted = emptydir(Path::new("."));
 
     match count_deleted {
         0 => println!("{}", "No empty directories found".blue()),
