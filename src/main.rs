90cd9c916eda1ad0ceaa6a1ace3fd3f5f84f71a8
Alex Chan <alex@alexwlchan.net>
2024-06-18 21:37:30 +0100

    Add colour printing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![deny(warnings)]
 
+use colored::*;
 use walkdir::WalkDir;
 
 mod can_be_deleted;
@@ -12,8 +13,20 @@ fn main() -> Result<(), std::io::Error> {
         .filter(|e| e.file_type().is_dir())
         .filter(|e| can_be_deleted::can_be_deleted(e.path()));
 
+    let mut count_deleted = 0;
+
     for entry in iterator {
         println!("{}", entry.path().display());
+        count_deleted += 1;
+    }
+
+    match count_deleted {
+        0 => println!("{}", "No empty directories found".blue()),
+        1 => println!("{}", "1 directory deleted".green()),
+        _ => {
+            let message = format!("{} directories deleted", count_deleted);
+            println!("{}", message.green());
+        }
     }
 
     Ok(())
