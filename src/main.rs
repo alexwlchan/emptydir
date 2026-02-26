e0c24bfcd4d7ec5ee1007d799f1069d2b5b68354
Alex Chan <alexc@tailscale.com>
2026-02-26 06:09:32 -0800

    cli: allow running `emptydir .` to delete current dir
    Plus start adding some CLI tests.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![deny(warnings)]
 
+use std::fs;
 use std::path::Path;
+use std::process;
 
 use clap::Parser;
 use colored::*;
@@ -20,17 +22,21 @@ struct Cli {
 fn main() -> Result<(), std::io::Error> {
     let cli = Cli::parse();
 
-    let root = Path::new(&cli.root);
-    let result = emptydir::emptydir(root);
+    let root = fs::canonicalize(Path::new(&cli.root))?;
+    let result = emptydir::emptydir(&root);
 
     match (result.count_deleted, result.count_errors) {
         (0, 0) => match can_be_deleted::can_be_deleted(&root) {
             can_be_deleted::DeleteDecision::CannotDelete(reason) => {
                 eprintln!("{}", reason.to_string().red());
+                process::exit(1);
             }
             _ => (),
         },
-        (0, _) => println!("{}", "Unable to delete empty directories".red()),
+        (0, _) => {
+            println!("{}", "Unable to delete empty directories".red());
+            process::exit(1);
+        }
         (1, _) => println!("{}", "1 directory deleted".green()),
         _ => {
             let message = format!(
@@ -42,4 +48,62 @@ fn main() -> Result<(), std::io::Error> {
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod test_emptydir {
+    use std::fs;
+    use std::path::PathBuf;
+
+    use assert_cmd::Command;
+    use predicates::prelude::*;
+
+    fn test_dir() -> PathBuf {
+        let tmp_dir = tempfile::tempdir().unwrap();
+        let path = tmp_dir.path();
+        path.to_owned()
+    }
+
+    fn create_dir(dir: &PathBuf) {
+        fs::create_dir_all(dir).unwrap();
+    }
+
+    #[expect(
+        deprecated,
+        reason = "cargo_bin is deprecated, cargo_bin! is not, `use` does not differenciate them"
+    )]
+    #[test]
+    fn it_prints_the_version() {
+        // Match strings like `emptydir 1.2.3`
+        let is_version_string =
+            predicate::str::is_match(r"^emptydir [0-9]+\.[0-9]+\.[0-9]+\n$").unwrap();
+
+        Command::cargo_bin("emptydir")
+            .unwrap()
+            .arg("--version")
+            .assert()
+            .success()
+            .stdout(is_version_string)
+            .stderr("");
+    }
+
+    #[expect(
+        deprecated,
+        reason = "cargo_bin is deprecated, cargo_bin! is not, `use` does not differenciate them"
+    )]
+    #[test]
+    fn it_deletes_dot_directory() {
+        let dir = test_dir();
+
+        // Create the directory, but don't put anything in it
+        create_dir(&dir);
+
+        Command::cargo_bin("emptydir")
+            .unwrap()
+            .current_dir(&dir)
+            .arg(".")
+            .assert()
+            .success();
+        assert_eq!(dir.exists(), false);
+    }
 }
